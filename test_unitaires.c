d49015f3d6aa192d8d5ffbeb973ee66df3ea1c73
YoannGouet <yoann.gouet@hotmail.fr>
2017-02-26 22:52:00 +0100

    ajout des test sur couleur

--- test_unitaires.c
+++ test_unitaires.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include "grille.h"
-
+#include "couleur.h"
 
 void grille_couleur_init_aleatoire(grille g, int taille){
     int i;  
@@ -54,8 +54,69 @@ int main()
     /*Test de la creation de la grille a partir de la fonction*/
     grille_couleur_init_fichier(g, fp, taille);
 
+    
+    grille h=Grille(taille);
+    init_grille(h,taille);
+    /*on change chaque case de h une par une pour la metre en rouge*/
+    for ( i = 0; i < taille*taille; i++) {
+      change1(h,i,'R');
+    }
+    printf("\n" );
+    for ( i = 0; i < taille*taille; i++) {
+      printf("%c ",get_couleur1(h[i]));
+      if(i%taille==taille-1)
+          printf("\n");
+    }
+    liste k=NULL;
+    k=makel(0,k);
+    k=composante(h,k,taille);
+    
+    /*la liste est initialisée a 0*/
+    liste l=NULL;
+    l=makel(0,l);
+    
+    /*on recupere la composante connexe*/
+    l=composante(g,l,taille);
+    
+    /*on affiche la liste et la grille pour verifier*/
+    k=composante(g,l,taille);
+    while (!(listevide(k))) {
+      printf("%d ",pop(k));
+      k=sui(k);
+    }
+    printf("\n" );
+     for ( i = 0; i < taille*taille; i++) {
+      printf("%c ",get_couleur1(g[i]));
+      if(i%taille==taille-1)
+          printf("\n");
+    }
+    printf("\n" );
+    
+    /*on utilise changeall et verifie le resultat*/
+    changeall(g,l,'M');
+    for ( i = 0; i < taille*taille; i++) {
+      printf("%c ",get_couleur1(g[i]));
+      if(i%taille==taille-1)
+          printf("\n");
+    }
+    printf("\n" );
+    l=composante(g,l,taille);
+    changeall(g,l,'B');
+    for ( i = 0; i < taille*taille; i++) {
+      printf("%c ",get_couleur1(g[i]));
+      if(i%taille==taille-1)
+          printf("\n");
+    }
+    
+    /*on teste win sur h et g*/
+    printf("victoire sur g %d\n",win(g,taille) );
+    printf("victoire sur h %d\n",win(h,taille) );
+    
     /*Liberation de la memoire*/
     liberation(g);
+    liberation(h);
+    freel(l);
+    freel(k);
 
     return 0;
 }
