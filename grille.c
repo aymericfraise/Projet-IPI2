0ae46c4c12626198ee35021210438f0b818160a2
aymericfraise <aymericfraise@hotmail.fr>
2017-03-10 12:18:23 +0100

    doc grille.c -> grille.h

--- grille.c
+++ grille.c
@@ -18,41 +18,26 @@ struct Case
 {
      char couleur;
      int dedans;
-};   
+};
 
-/**
-    \fn Grille(int taille)
-    \param la taille 
-    \return Retourne un pointeur qui pointe la première case de grille.
-**/
+
 grille Grille(int taille)
 {
      grille ret=NULL;
-     ret=calloc(taille*taille,sizeof(Case));	
+     ret=calloc(taille*taille,sizeof(Case));
      return ret;
 }
 
-/**
-    \fn liberation(grille a)
-    \param la grille a 
-    \return rien
-    \brief Permet de libérer la memoire pointé par le pointeur a
-**/
+
 void liberation(grille a)
 {
      free(a);
 }
 
-/**
-    \fn constructeur()
-    \param rien
-    \return la couleur produite aléatoirement.
-    \brief Permet de produire une couleur aléatoire
-**/
 
 char constructeur()
 {
-    int i=random()%6+1;     
+    int i=random()%6+1;
     char couleur;
     switch(i){
         case 1:couleur='B';break;
@@ -65,90 +50,46 @@ char constructeur()
     }
     return couleur;
 }
- 
-/**
-    \fn get_couleur(grille g)
-    \param Le pointeur sur la grille, éventuellement incrémenté pour accéder à une autre case que la première.
-    \return Retourne un char correspondant à la couleur de la case.
-    \brief Permet de récupérer la couleur de la case pointée par le pointeur g.
-**/
+
+
 char get_couleur(grille g) {
     return (g->couleur);
 }
-
-/**
-    \fn get_dedans(grille g)
-    \param grille g
-    \return la valeur de dedans
-    \brief Permet d'obtenir le dedans de la case pointé par le pointeur g
-**/
 
 int get_dedans(grille g) {
     return (g->dedans);
 }
 
-/**
-    \fn aug_g(grille g,int distance)
-    \param grille g,int distance
-    \return le pointeur
-    \brief Permet d'augmenter la valeur de pointeur
-**/
 grille aug_g(grille g,int distance)
 {
     g=g+distance;
     return (g);
 }
 
-/**
-    \fn change1(grille g,int taille,char couleur)
-    \param grille g,int taille,char couleur
-    \return rien
-    \brief Permet de changer la couleur d'indice taille
-**/
 void change1(grille g,int taille,char couleur)
 {
        g=aug_g(g,taille);
        g->couleur=couleur;
 }
 
-/**
-    \fn change_dedans(grille g,int taille,int dedans)
-    \param grille g,int taille,int dedans
-    \return rien
-    \brief Permet de changer la valeur d'indice taille
-**/
 void change_dedans(grille g,int taille,int dedans)
 {
       g=aug_g(g,taille);
       g->dedans=dedans;
 }
 
-
-
-/**
-    \fn init_grille(grille a,int taille)
-    \param grille a,int taille
-    \return rien
-    \brief Permet d'initialiser la grille par les couleurs aléatoires
-**/
 void init_grille(grille a,int taille)
 {
     int i=0;
     change_dedans(a,0,1);
     for(i=0;i<taille*taille;i++)
     {
-   
+
          a->couleur=constructeur();
          a=aug_g(a,1);
     }
 }
 
-/**
-    \fn cree_fichier_de_couleur(int taille)
-    \param int taille
-    \return rien
-    \brief Permet de créer un ficher qui contient les couleurs aléatoires
-**/
 void cree_fichier_de_couleur(int taille)
 {
     FILE *fp=fopen("couleur.dat","w+");
@@ -159,14 +100,7 @@ void cree_fichier_de_couleur(int taille)
     }
     fclose(fp);
 }
-    
 
-/**
-    \fn init_grille_fichier(grille a,FILE *fp,int taille)
-    \param grille a,FILE *fp,int taille
-    \return rien.
-    \brief Permet d'initialiser la grille par le fichier
-**/
 void init_grille_fichier(grille a,FILE *fp,int taille)
 {
      int i=0;
@@ -175,19 +109,5 @@ void init_grille_fichier(grille a,FILE *fp,int taille)
      {
          a[i].couleur=fgetc(fp);
      }
-     fclose(fp); 
+     fclose(fp);
 }
-
-
-
-
-
-
-     
-         
-
-
-
-
-
-
--- grille.h
+++ grille.h
@@ -1,31 +1,95 @@
 #include<time.h>
-#include <stdbool.h> 
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 /*définition de la structure de la grille*/
 typedef struct Case Case ,*grille;
- 
 
-/*créer une grille de taille int*/
+
+/**
+    \fn Grille(int taille)
+    \param la taille
+    \return Retourne un pointeur qui pointe la première case de grille.
+**/
 grille Grille(int taille);
-/*liberer la memoire pour la grille g*/
+
+/**
+    \fn liberation(grille a)
+    \param la grille a
+    \return rien
+    \brief Permet de libérer la memoire pointé par le pointeur a
+**/
 void liberation(grille g);
-/*créer une couleur aléatoire*/
+
+/**
+    \fn constructeur()
+    \param rien
+    \return la couleur produite aléatoirement.
+    \brief Permet de produire une couleur aléatoire
+**/
 char constructeur();
-/*initialisation de la grille par les couleurs aléatoires*/
+
+/**
+    \fn get_couleur(grille g)
+    \param Le pointeur sur la grille, éventuellement incrémenté pour accéder à une autre case que la première.
+    \return Retourne un char correspondant à la couleur de la case.
+    \brief Permet de récupérer la couleur de la case pointée par le pointeur g.
+**/
 void init_grille(grille ,int);
-/*obtenir la couleur de permiere case de grille g*/
+
+/**
+    \fn get_dedans(grille g)
+    \param grille g
+    \return la valeur de dedans
+    \brief Permet d'obtenir le dedans de la case pointé par le pointeur g
+**/
 char get_couleur(grille g);
-/*obtenir le dedans de permiere case de grille g*/
+
+/**
+    \fn aug_g(grille g,int distance)
+    \param grille g,int distance
+    \return le pointeur
+    \brief Permet d'augmenter la valeur de pointeur
+**/
 int get_dedans(grille g);
-/*augmenter le pointeur de grille g*/
+
+/**
+    \fn change1(grille g,int taille,char couleur)
+    \param grille g,int taille,char couleur
+    \return rien
+    \brief Permet de changer la couleur d'indice taille
+**/
 grille aug_g(grille g,int distance);
-/*creer le ficher de couleur*/
+
+/**
+    \fn change_dedans(grille g,int taille,int dedans)
+    \param grille g,int taille,int dedans
+    \return rien
+    \brief Permet de changer la valeur d'indice taille
+**/
 void change1(grille g,int taille,char couleur);
+
+/**
+    \fn init_grille(grille a,int taille)
+    \param grille a,int taille
+    \return rien
+    \brief Permet d'initialiser la grille par les couleurs aléatoires
+**/
 void change_dedans(grille g,int taille,int dedans);
+
+/**
+    \fn cree_fichier_de_couleur(int taille)
+    \param int taille
+    \return rien
+    \brief Permet de créer un ficher qui contient les couleurs aléatoires
+**/
 void cree_fichier_de_couleur(int);
-/*initialisation de la grille par les couleurs dans le  fichier*/
+
+/**
+    \fn init_grille_fichier(grille a,FILE *fp,int taille)
+    \param grille a,FILE *fp,int taille
+    \return rien.
+    \brief Permet d'initialiser la grille par le fichier
+**/
 void init_grille_fichier(grille ,FILE *,int );
-
-
