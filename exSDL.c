96a0ee4a355226115fe81ae320d55cba327ea13d
MinLiiiii <0906limin@gmail.com>
2017-03-24 12:47:00 +0100

    Add files via upload

--- exSDL.c
+++ exSDL.c
@@ -9,6 +9,7 @@
 
 int autoDraw=0;
 
+
 // px, py coordonnées haut, gauche du pixel
 void drawRectangle(SDL_Surface *ecran, int px, int py, int size, int r, int g, int b) {
     SDL_Rect rect;
@@ -49,17 +50,17 @@ void upi(char c,int i[]){
 void jouable(grille g,liste l,int i[6],int taille){
    if (!listevide(l)) {
     int a=pop(l);
-    if (a%taille!=0 && get_couleur(g,a-1)!=get_couleur(g,a)) {
-      upi(get_couleur(g,a-1),i);
+    if (a%taille!=0 && get_couleur(aug_g(g,a-1))!=get_couleur(aug_g(g,a))) {
+      upi(get_couleur(aug_g(g,a-1)),i);
     }
-    if (a%taille!=taille-1 && get_couleur(g,a+1)!=get_couleur(g,a)) {
-      upi(get_couleur(g,a+1),i);
+    if (a%taille!=taille-1 && get_couleur(aug_g(g,a+1))!=get_couleur(aug_g(g,a))) {
+      upi(get_couleur(aug_g(g,a+1)),i);
     }
-    if (a/taille!=0 && get_couleur(g,a-taille)!=get_couleur(g,a)) {
-      upi(get_couleur(g,a-taille),i);
+    if (a/taille!=0 && get_couleur(aug_g(g,a-taille))!=get_couleur(aug_g(g,a))) {
+      upi(get_couleur(aug_g(g,a-taille)),i);
     }
-    if (a/taille!=taille-1 && get_couleur(g,a+taille)!=get_couleur(g,a)) {
-      upi(get_couleur(g,a+taille),i);
+    if (a/taille!=taille-1 && get_couleur(aug_g(g,a+taille))!=get_couleur(aug_g(g,a))) {
+      upi(get_couleur(aug_g(g,a+taille)),i);
     }
      jouable(g,sui(l),i,taille);
   }
@@ -89,20 +90,27 @@ void printjouable(int j[6]){
  printf("\n");
 }
 
-void affiche_SDL(grille g,int taille,SDL_Surface *ecran/*,int coup*/) {
+void affiche_SDL(grille g,int taille,SDL_Surface *ecran,int sz_rect)
+{
    int i,ligne,colonne;
    char couleur;
    for(i=0;i<taille*taille;i++)
          {
-             couleur=get_couleur(g,i);
+             couleur=get_couleur(aug_g(g,i));
              switch (couleur) {
-             case 'B':{ligne=i/taille;colonne=i%taille;drawRectangle(ecran, ligne*50, colonne*50, 50, 0, 0, 255);break;}
-             case 'V':{ligne=i/taille;colonne=i%taille;drawRectangle(ecran, ligne*50, colonne*50, 50, 0, 100, 0);break;}
-             case 'R':{ligne=i/taille;colonne=i%taille;drawRectangle(ecran, ligne*50, colonne*50, 50, 255, 0, 0);break;}
-             case 'J':{ligne=i/taille;colonne=i%taille;drawRectangle(ecran, ligne*50, colonne*50, 50, 255, 185,15);break;}
-             case 'M':{ligne=i/taille;colonne=i%taille;drawRectangle(ecran, ligne*50, colonne*50, 50, 139, 69, 19);break;}
-             case 'G':{ligne=i/taille;colonne=i%taille;drawRectangle(ecran, ligne*50, colonne*50, 50, 139, 87, 66);break;}
+             case 'B':{ligne=i/taille;colonne=i%taille;drawRectangle(ecran, ligne*sz_rect, colonne*sz_rect, sz_rect, 0, 0, 255);break;}
+             case 'V':{ligne=i/taille;colonne=i%taille;drawRectangle(ecran, ligne*sz_rect, colonne*sz_rect, sz_rect, 0, 100, 0);break;}
+             case 'R':{ligne=i/taille;colonne=i%taille;drawRectangle(ecran, ligne*sz_rect, colonne*sz_rect, sz_rect, 255, 0, 0);break;}
+             case 'J':{ligne=i/taille;colonne=i%taille;drawRectangle(ecran, ligne*sz_rect, colonne*sz_rect, sz_rect, 255, 185,15);break;}
+             case 'M':{ligne=i/taille;colonne=i%taille;drawRectangle(ecran, ligne*sz_rect, colonne*sz_rect, sz_rect, 139, 69, 19);break;}
+             case 'G':{ligne=i/taille;colonne=i%taille;drawRectangle(ecran, ligne*sz_rect, colonne*sz_rect, sz_rect, 139, 87, 66);break;}
              }
           }
-    /*printf("nombre de coup restant:%d\n",coup );*/
 }
+
+
+
+
+
+
+
